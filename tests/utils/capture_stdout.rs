bcbff906996491668154429c5cddab8128327ada
Kyaw Zin Min <kyawzinmin.dev@protonmail.com>
2026-06-02 01:22:57 +0700

    /tests/utils/capture_stdout.rs: separated the stdout into a new thread

--- tests/utils/capture_stdout.rs
+++ tests/utils/capture_stdout.rs
@@ -4,19 +4,25 @@ use std::os::unix::io::FromRawFd;
 pub fn capture_stdout<F: FnOnce()>(f: F) -> Vec<u8> {
 	let	mut fds = [-1i32; 2];
 	unsafe { libc::pipe(fds.as_mut_ptr()); }
+
 	let saved = unsafe { libc::dup(1) };
 	unsafe {
 		libc::dup2(fds[1], 1);
 		libc::close(fds[1]);
 	}
+
+	let reader_thread = std::thread::spawn(move || {
+		let mut reader = unsafe { std::fs::File::from_raw_fd(fds[0]) };
+		let mut output = Vec::new();
+		reader.read_to_end(&mut output).unwrap();
+		output
+	});
+
 	f();
-	unsafe { libc::fsync(1); }
+
 	unsafe {
 		libc::dup2(saved, 1);
 		libc::close(saved);
 	}
-	let mut reader = unsafe { std::fs::File::from_raw_fd(fds[0]) };
-	let mut output = Vec::new();
-	reader.read_to_end(&mut output).unwrap();
-	output
+	reader_thread.join().unwrap()
 }
