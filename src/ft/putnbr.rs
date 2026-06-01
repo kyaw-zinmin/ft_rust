aa0fa771dc8e43e2abad5d5fcf8438188a36b56c
Kyaw Zin Min <kyawzinmin.dev@protonmail.com>
2026-06-01 16:47:46 +0700

    /ft/putnbr.rs: changed the type of n from i32 to &i32, to be compatible with /tests/compare_cases.rs

--- src/ft/putnbr.rs
+++ src/ft/putnbr.rs
@@ -1,7 +1,8 @@
 use crate::ft;
 use libc::{c_void, write};
 
-pub	fn putnbr(mut nbr: i32) {
+pub	fn putnbr(n: &i32) {
+	let mut nbr = *n;
 	if nbr == i32::MIN {
 		ft::putstr("-2147483648");
 		return ;
@@ -11,13 +12,13 @@ pub	fn putnbr(mut nbr: i32) {
 		nbr = -nbr;
 	}
 	if nbr >= 10 {
-		putnbr(nbr / 10);
+		putnbr(&(nbr / 10));
 	}
 	let c = b'0' + (nbr % 10) as u8;
 	ft::putchar(&c);
 }
 
-pub	fn putnbrln(nbr: i32) {
+pub	fn putnbrln(nbr: &i32) {
 	putnbr(nbr);
 	let nl = [b'\n'].as_ptr() as *const c_void;
 	unsafe { write(1, nl, 1); }
