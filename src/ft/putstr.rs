cfb642e7089cb63de1c9776167432dd981779245
Kyaw Zin Min <kyawzinmin.dev@protonmail.com>
2026-06-01 16:09:28 +0700

    reverted the decision

--- src/ft/putstr.rs
+++ src/ft/putstr.rs
@@ -1,12 +1,13 @@
 use libc::{c_void, write};
 
 pub	fn putstr(s: &str) {
+	let len = s.len();
 	let s = s.as_ptr() as *const c_void;
-	unsafe { write(1, s, 1); }
+	unsafe { write(1, s, len); }
 }
 
 pub fn putstrln(s: &str) {
 	putstr(s);
-	let nl = b'\n' as *const u8 as *const c_void;
+	let nl = [b'\n'].as_ptr() as *const c_void;
 	unsafe { write(1, nl, 1); }
 }
