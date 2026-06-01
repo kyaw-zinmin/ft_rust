23fc9f09c0aa0f50a13e728cf4d25b82c290787f
Kyaw Zin Min <kyawzinmin.dev@protonmail.com>
2026-06-01 17:24:29 +0700

    /tests/utils/compare_cases.rs: created a custom trait for expected so that it can be more flexible in matching according to the type of itself. @var{out} from capture_stdout will always be Vec<u8>, so if @var{expected} is &str, compare as_bytes(), if @var{expected} is i32, parse @var{out} to i32 and compare

--- tests/utils/compare_cases.rs
+++ tests/utils/compare_cases.rs
@@ -1,15 +1,31 @@
 use crate::utils;
 
+pub trait MatchesOutput {
+	fn matches(&self, out: &[u8]) -> bool;
+}
+impl MatchesOutput for str {
+	fn matches(&self, out: &[u8]) -> bool {
+		out == self.as_bytes()
+	}
+}
+impl MatchesOutput for i32 {
+	fn matches(&self, out: &[u8]) -> bool {
+		let s = String::from_utf8_lossy(out);
+		let s = s.trim_end_matches('\n');
+		s.parse::<i32>().ok() == Some(*self)
+	}
+}
+
 #[allow(dead_code)]
 pub fn	compare_cases<T, E>(cases: &[(&str, &T, &E)], f: fn(&T)) 
 where
 	T: std::fmt::Display + ?Sized,
-	E: AsRef<[u8]> + ?Sized
+	E: MatchesOutput + ?Sized
 {
 	for (i, &(desc, input, expected))
 	in cases.iter().enumerate() {
 		utils::print_case((i + 1) as i32, desc);
 		let out: Vec<u8> = utils::capture_stdout(|| f(input));
-		assert_eq!(out, expected.as_ref(), "failed for {}", input);
+		assert!(expected.matches(&out), "failed for {}", input);
 	}
 }
