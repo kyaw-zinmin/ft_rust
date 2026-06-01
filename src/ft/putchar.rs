90cb415954ac95873a5ee7a155c79d5884d604f0
Kyaw Zin Min <kyawzinmin.dev@protonmail.com>
2026-06-01 16:00:25 +0700

    modified for readibility

--- src/ft/putchar.rs
+++ src/ft/putchar.rs
@@ -1,13 +1,8 @@
 use libc::{c_void, write};
 
 pub fn putchar(c: &u8) {
-	unsafe {
-		write(
-			1, 
-			c as *const u8 as *const c_void, 
-			1
-		);
-	}
+	let c = c as *const u8 as *const c_void;
+	unsafe { write(1, c, 1); }
 }
 
 pub fn putcharln(c: u8) {
