ee90c05c12680183b1e12dbb84459238d5a5cff3
Kyaw Zin Min <kyawzinmin.dev@protonmail.com>
2026-06-08 16:21:09 +0700

    tests/fortytwo.rs: example usage of /utils/red_assert_eq.rs

--- tests/fortytwo.rs
+++ tests/fortytwo.rs
@@ -1,9 +1,6 @@
 mod utils;
 use ft_rust::ft;
 
-const RED: &str = "\x1b[31m";
-const RESET: &str = "\x1b[0m";
-
 #[test]
 fn test_fortytwo() {
 	let expected = 42;
@@ -12,7 +9,6 @@ fn test_fortytwo() {
 	for (i, (label, num)) in labels.iter().zip(&mut nums).enumerate() {
 		utils::print_case((i + 1) as i32, *label);
 		ft::ft(num);
-		print!("{}", RED);
-		assert_eq!(expected, *num, "Failed for:{} {}", RESET, num);
+		utils::red_assert_eq(&expected, num);
 	}
 }
