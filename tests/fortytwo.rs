3af7b8fdbd540ceb3b05eff44687143d7e6ee6ee
Kyaw Zin Min <kyawzinmin.dev@protonmail.com>
2026-06-08 15:07:15 +0700

    changed to use utils::print_case for mroe readibiilty

--- tests/fortytwo.rs
+++ tests/fortytwo.rs
@@ -10,11 +10,7 @@ fn test_fortytwo() {
 	let labels = ["P Max", "N Max", "0", "42"];
 	let mut nums = [2147483647, -2147483648, 0, 42];
 	for (i, (label, num)) in labels.iter().zip(&mut nums).enumerate() {
-		ft::putstr("Case #");
-		let i = (i + 1) as i32;
-		ft::putnbr(&i);
-		ft::putstr(": ");
-		ft::putstrln(*label);
+		utils::print_case((i + 1) as i32, *label);
 		ft::ft(num);
 		print!("{}", RED);
 		assert_eq!(expected, *num, "Failed for:{} {}", RESET, num);
