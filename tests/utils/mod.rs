5ca552e6b0e5c2e340de2ace9b1f501b5b3f1713
Kyaw Zin Min <kyawzinmin.dev@protonmail.com>
2026-06-08 16:20:23 +0700

    modified for red_assert_eq.rs

--- tests/utils/mod.rs
+++ tests/utils/mod.rs
@@ -2,6 +2,7 @@ mod capture_stdout;
 mod print_case;
 mod compare_cases;
 mod compare_output;
+mod red_assert_eq;
 
 pub use capture_stdout::*;
 pub use print_case::*;
@@ -9,3 +10,5 @@ pub use print_case::*;
 pub use compare_cases::*;
 #[allow(unused_imports)]
 pub use compare_output::*;
+#[allow(unused_imports)]
+pub use red_assert_eq::*;
